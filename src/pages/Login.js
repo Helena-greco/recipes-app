5c85e2516d7f551c3b950d1bfddaf4ff630936c2
Bernardo Raffide <bernardoraffide@pop-os.localdomain>
2021-11-05 16:36:30 -0300

    pagina de login concluida, sem testes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 
 const six = 6;
 
@@ -7,15 +8,19 @@ function Login() {
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
 
+  const history = useHistory();
+
   const isEmailValid = (ameil) => {
     const regexEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
     return regexEmail.test(ameil);
   };
 
-  const handleClick = () => {
+  const handleClick = (event) => {
+    event.preventDefault();
     localStorage.setItem('user', JSON.stringify({ email }));
     localStorage.setItem('mealsToken', 1);
     localStorage.setItem('cocktailsToken', 1);
+    history.push('/comidas');
   };
 
   return (
