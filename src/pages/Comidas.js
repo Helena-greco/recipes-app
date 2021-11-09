9e093ef463bcdbb1aad0bcd89e06303631e339ed
Fernando Serpa <serpaef@outlook.com>
2021-11-09 20:31:42 -0300

    refact: lógica de filtragem dos botões

--- src/pages/Comidas.js
+++ src/pages/Comidas.js
@@ -1,17 +1,22 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import Header from '../components/Header';
 import CardList from '../components/CardList';
 import Categories from '../components/Categories';
 import RecipesContext from '../context/RecipesContext';
 
 const COMIDAS = 'Comidas';
-
-const URL = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+const MAX_CAT = 5;
 
 function Comidas() {
-  const { categories, filterMeals, fetchCategories } = useContext(RecipesContext);
+  const { filterMeals } = useContext(RecipesContext);
+  const [categories, setCategories] = useState([]);
+  const fetchCategories = async () => {
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+    const result = await response.json();
+    setCategories([...result.meals].slice(0, MAX_CAT));
+  };
 
-  useEffect(() => { fetchCategories(URL); }, []);
+  useEffect(() => { fetchCategories(); }, []);
 
   return (
     <div>
