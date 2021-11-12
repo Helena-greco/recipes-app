119fded173d8d89fcc9252b8b44a4c33938ca980
Fernando Serpa <serpaef@outlook.com>
2021-11-11 21:30:33 -0300

    impl: pagina explore

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+const EXPLORAR = 'Explorar';
+
 function Explorar() {
-  const explorar = 'Explorar';
+  const history = useHistory();
+
+  const handleClick = (route) => {
+    history.push(`/explorar/${route}`);
+  };
+
   return (
     <>
-      <Header title={ explorar } search={ false } />
+      <Header title={ EXPLORAR } search={ false } />
+      <Button
+        type="button"
+        data-testid="explore-food"
+        onClick={ () => { handleClick('comidas'); } }
+      >
+        Explorar Comidas
+      </Button>
+      <Button
+        type="button"
+        data-testid="explore-drinks"
+        onClick={ () => { handleClick('bebidas'); } }
+      >
+        Explorar Bebidas
+      </Button>
       <Footer />
     </>
   );
