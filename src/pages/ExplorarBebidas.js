f93622ef8373c19343cf17382e4ae4fc6cca747d
Fernando Serpa <serpaef@outlook.com>
2021-11-11 22:43:06 -0300

    impl: botões pagina explore

--- src/pages/ExplorarBebidas.js
+++ src/pages/ExplorarBebidas.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+const EXPLORAR = 'Explorar Bebidas';
+
 function ExplorarBebidas() {
-  const explorar = 'Explorar Bebidas';
+  const history = useHistory();
+
+  const handleClick = (route) => {
+    history.push(`/explorar/bebidas/${route}`);
+  };
+
   return (
     <>
-      <Header title={ explorar } search={ false } />
+      <Header title={ EXPLORAR } search={ false } />
+      <Button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ (() => { handleClick('ingredientes'); }) }
+      >
+        Por Ingredientes
+
+      </Button>
+      <Button type="button" data-testid="explore-surprise">Me Surpreenda!</Button>
       <Footer />
     </>
   );
--- src/pages/ExplorarComidas.js
+++ src/pages/ExplorarComidas.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+const EXPLORAR = 'Explorar Comidas';
+
 function ExplorarComidas() {
-  const explorar = 'Explorar Comidas';
+  const history = useHistory();
+
+  const handleClick = (route) => {
+    history.push(`/explorar/comidas/${route}`);
+  };
+
   return (
     <>
-      <Header title={ explorar } search={ false } />
+      <Header title={ EXPLORAR } search={ false } />
+      <Button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => { handleClick('ingredientes'); } }
+      >
+        Por Ingredientes
+      </Button>
+      <Button
+        type="button"
+        data-testid="explore-by-area"
+        onClick={ () => { handleClick('area'); } }
+      >
+        Por Local de Origem
+      </Button>
+      <Button
+        type="button"
+        data-testid="explore-surprise"
+      >
+        Me Surpreenda!
+      </Button>
       <Footer />
     </>
   );
